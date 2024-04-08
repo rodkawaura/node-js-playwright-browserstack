4994054fe0f8d0f3b6bff0c24055112ba76f603b
rodkawaura <rkmontemor@gmail.com>
2024-04-08 17:44:50 -0300

    Test

---
+++ tests/porto_test.js
@@ -0,0 +1,9 @@
+const { expect, test } = require('@playwright/test');
+
+test('Acessar Porto', async ({ page }) => {
+  // visit the site
+  await page.goto('https://www.portoseguro.com.br/');
+
+  await page.click("//span[text()='Área do Cliente']");
+
+});
