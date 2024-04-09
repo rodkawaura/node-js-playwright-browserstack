cb6e954e7281379c44622919d9d5a113bef2bf94
rodkawaura <rkmontemor@gmail.com>
2024-04-09 17:10:32 -0300

    .

--- tests/porto_test.js
+++ tests/porto_test.js
@@ -1,6 +1,25 @@
 const { expect, test } = require('@playwright/test');
 
+
+
 test('Acessar Porto', async ({ page }) => {
+
+  var username = process.env.BROWSERSTACK_USERNAME;
+  var accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
+  var buildName = process.env.BROWSERSTACK_BUILD_NAME;
+
+  var capabilities = {
+    "os": "Windows",
+    "os_version": "10",
+    "browser": "chrome",
+    "browser_version": "latest",
+    "name": "BStack -[Jenkins] Sample Test", // test name
+    "build": buildName, // CI/CD job name using BROWSERSTACK_BUILD_NAME env variable
+    "browserstack.user": username,
+    "browserstack.key": accessKey
+  };
+
+
 
   // visit the site
   await page.goto('https://www.portoseguro.com.br/');
